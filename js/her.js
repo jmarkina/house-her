0454be4fa759962278328fe30d887343fb066ab2
Yulia Markina <jmarkina@gmail.com>
2017-07-09 11:08:27 -0700

    display data

--- js/her.js
+++ js/her.js
@@ -2,11 +2,11 @@
 
 var story_data = storydata;
  
-
 for (var key in story_data){
     // loop over all stories and save to div
-    console.log(story_data[key].story);
-    console.log(key);
-
-;};
+    var allStories;
+    allStories = "<div class='each-story'> " + story_data[key].story + "</div>";
+    console.log(allStories);
+    document.write(allStories);
+};
 
