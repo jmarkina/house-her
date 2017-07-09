b447b5679d323f5af9795f16538ceb1301d68051
vagrant <vagrant@vagrant.vm>
2017-07-09 18:11:05 +0000

    merge

--- js/her.js
+++ js/her.js
@@ -5,8 +5,15 @@ var story_data = storydata;
 
 for (var key in story_data){
     // loop over all stories and save to div
-    console.log(story_data[key].story);
+    console.log(story+key);
+    // $('#story' + key).append(story_data[key].story);
+    var new_story = console.log(story_data[key].story);
     console.log(key);
 
-;};
+}
+
+
+
+
+
 
